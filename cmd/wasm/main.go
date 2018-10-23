2d90f55b812ec52743d80e1aacb8c14f9bf39d3f
David Sauer <davedamoon@gmail.com>
2018-10-23 23:53:48 +0200

    continue loading data in case of error

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -1,36 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"syscall/js"
 	"time"
 )
 
 func main() {
+	for {
+		updateJsonLoop()
+	}
+}
 
-	go updateJson()
-
-	select {}
+func updateJsonLoop() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("recovered: %v\n", err)
+		}
+	}()
+	t := time.NewTicker(1 * time.Second)
+	for range t.C {
+		updateJson()
+	}
 }
 
 func updateJson() {
-	t := time.NewTicker(1 * time.Second)
-	for range t.C {
-		//
-		resp, err := http.Get("./status.json")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
-		b, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		//	enc := base64.StdEncoding.EncodeToString(b)
-
-		el := js.Global().Get("document").Call("getElementById", "thing")
-		el.Set("innerHTML", string(b))
+	resp, err := http.Get("./status.json")
+	if err != nil {
+		fmt.Printf("updatejson: %v\n", err)
 	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("updatejson: %v\n", err)
+	}
+
+	el := js.Global().Get("document").Call("getElementById", "thing")
+	el.Set("innerHTML", string(b))
 }
