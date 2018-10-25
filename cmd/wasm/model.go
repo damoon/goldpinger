b98ba1413f14d41d92c0777f703cfc0ebd772e83
David Sauer <david.sauer@turbinekreuzberg.com>
2018-10-25 13:42:26 +0200

    added table to display measurement

--- cmd/wasm/model.go
+++ cmd/wasm/model.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"strconv"
 	"syscall/js"
+	"text/template"
 
 	"github.com/damoon/goldpinger/pkg"
 	"github.com/mohae/deepcopy"
@@ -28,14 +31,98 @@ func startNewModel() ModelAgent {
 	go func() {
 		for f := range c {
 			f(m)
-			el := js.Global().Get("document").Call("getElementById", "thing")
-			el.Set("innerHTML", m.Render())
+			d := js.Global().Get("document")
+			d.Call("getElementById", "measurement").Set("innerHTML", m.RenderMeasurement())
+			d.Call("getElementById", "json").Set("innerHTML", m.RenderJson())
 		}
 	}()
 	return c
 }
 
-func (m *Model) Render() string {
+const measurementsTemplate = `<table>
+<tr>
+	<td/>
+	{{ range $to := $.Nodes }}
+	<td ><div class="to">to {{ $to.HostName }}<div></td>
+	{{ end }}
+</tr>
+{{ range $from := $.Nodes }}
+<tr>
+	<td>from {{ $from.HostName }}</td>
+	{{ range $to := $.Nodes }}
+		{{ $m := index $.Measurements $from.HostName $to.HostName }}
+
+		{{ if $m }}
+			{{ if ne $m.Error "" }}
+				<td><img title="{{ $m.Error }}" src="./alert.png" /></td>
+			{{ else }}
+				{{ $ms := NanoToMilli $m.Delay }}
+				<td class="ping" style="color:{{ Color $ms }};">{{ printf "%.1f" $ms }}</td>
+			{{ end }}
+		{{ else }}
+			<td class="empty ping" />
+		{{ end }}
+
+	{{ end }}
+</tr>
+{{ end }}
+
+
+</table>
+`
+
+func (m *Model) RenderMeasurement() string {
+
+	nanoToMlli := func(n int64) float64 {
+		return float64(n) / 1000000
+	}
+
+	color := func(delay float64) string {
+		r := 0
+		g := 0
+		b := 0
+
+		if delay > 4 {
+			r = 255
+		} else {
+			r = int(255 / 4 * delay)
+		}
+
+		if delay < 4 {
+			g = 255
+		} else {
+			if delay > 8 {
+				g = 0
+			} else {
+				g = int(255 / 1 * (8 - delay))
+			}
+		}
+		return "rgb(" + strconv.Itoa(r) + ", " + strconv.Itoa(g) + ", " + strconv.Itoa(b) + ")"
+	}
+
+	fns := template.FuncMap{"NanoToMilli": nanoToMlli, "Color": color}
+
+	tpl, err := template.New("measurements").Funcs(fns).Parse(measurementsTemplate)
+	if err != nil {
+		return fmt.Sprintf("failed to parse measurements template: %v", err)
+	}
+
+	b := &bytes.Buffer{}
+	err = tpl.Execute(b, m.Model)
+	if err != nil {
+		return fmt.Sprintf("failed to render measurements template: %v", err)
+	}
+
+	return b.String()
+}
+
+func (m *Model) RenderJson() string {
+
+	// hacky disable, sorry
+	if true {
+		return ""
+	}
+
 	json, err := json.Marshal(m)
 	if err != nil {
 		return fmt.Sprintf("failed to marshal model to json: %v", err)
